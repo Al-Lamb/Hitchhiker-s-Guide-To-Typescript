f0afefa0a76119eace180eacc210d6de2a167c81
Alistair Lambert <lambert.alistair96@gmail.com>
2025-02-20 11:51:52 +0000

    Now we're getting there

---
+++ blogtothegalaxy/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Welcome to Typescript, come on in!</p>
+      <p>
+        Have a look around at the side bar to navigate to your chosen area of TS
+      </p>
+    </div>
+  );
+}
